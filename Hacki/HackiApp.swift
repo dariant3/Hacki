3b3cd420ccc94b91157bece42289ee1098cf63ab
Darian <dariant3@gmail.com>
2022-03-13 11:59:01 -0700

    Initial Commit

---
+++ Hacki/HackiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackiApp.swift
+//  Hacki
+//
+//  Created by Darian Tichler on 2022-03-13.
+//
+
+import SwiftUI
+
+@main
+struct HackiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
